6899508bea797c3d4f6e4a3102ed40d20483d050
popay97 <5880963+popay97@users.noreply.github.com>
2023-03-26 21:43:02 +0200

    /..

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/getEasyJetFilesFromFtp', async (req, res) => {
             username: 'dmc_cosmo',
             password: '~f0q/ugRR*K]'
         });
-        const list = await sftp.list('/dmc_cosmo/Cosmo/outgoing');
+        const list = await sftp.list('/dmc_cosmo/Cosmo/outgoing/live');
         console.log(list, 'the list of files')
         return res.status(200).send(list);
     } catch (err) {
