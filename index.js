dc3fdb968a8fcc16ec9f0a7ff41dea1fe9c3a25e
popay97 <5880963+popay97@users.noreply.github.com>
2023-03-26 22:47:14 +0200

    ..

--- index.js
+++ index.js
@@ -9,35 +9,28 @@ const openpgp = require('openpgp');
 const Client = require('ssh2-sftp-client');
 const sftp = new Client();
 
+const privateKeyPath = './private_key.asc'; // Update with the actual path to your private key file
+const publicKeyPath = '/pub_key.asc'; // Update with the actual path to your public key file
 
 const decryptFile = async (encryptedFilePath, privateKeyPath, publicKeyPath) => {
-    let decryptedFilePath;
-    try {
-        let encryptedMessage = fs.createReadStream(encryptedFilePath);
-        encryptedMessage = await openpgp.message.read(encryptedMessage);
-        let privateKey = await fs.promises.readFile(path.join(__dirname, privateKeyPath));
-        privateKey = await openpgp.key.readArmored(privateKey);
-        var passphrase = `COSMpass`;
-        await privateKey.keys[0].decrypt(passphrase);
-        let publicKey = await fs.promises.readFile(path.join(__dirname, publicKeyPath));
-        publicKey = await openpgp.key.read(publicKey);
-        let options = {
-            message: encryptedMessage,
-            publicKeys: publicKey.keys,
-            privateKeys: privateKey.keys
-        }
-        let decryptedMessage = await openpgp.decrypt(options);
-        //decryptedMessage.data is a NodeReadableStream so we need to convert it to a string before we can use it
-        decryptedMessage = await streamWait(decryptedMessage.data);
-        decryptedFilePath = path.join(__dirname, 'decryptedFile.txt');
-        fs.writeFileSync(decryptedFilePath, decryptedMessage);
+    const encryptedData = await fs.promises.readFile(encryptedFilePath);
+    const privateKey = await fs.promises.readFile(privateKeyPath, 'utf-8');
+    const publicKey = await fs.promises.readFile(publicKeyPath, 'utf-8');
 
-        return decryptedFilePath;
-    } catch (err) {
-        console.log(err)
-    }
+    const privateKeyObj = await openpgp.Key.readArmored(privateKey);
+    await privateKeyObj.keys[0].decrypt('COSMpass');
 
-    return decryptedFilePath;
+    const publicKeyObj = await openpgp.Key.readArmored(publicKey);
+
+    const options = {
+        message: await openpgp.Message.read(encryptedData),
+        publicKeys: publicKeyObj.keys,
+        privateKey: privateKeyObj.keys[0],
+    };
+
+    const decrypted = await openpgp.decrypt(options);
+
+    return decrypted.data;
 };
 
 app.get('/getEasyJetFilesFromFtp', async (req, res) => {
@@ -51,29 +44,20 @@ app.get('/getEasyJetFilesFromFtp', async (req, res) => {
         const list = await sftp.list('/dmc_cosmo/Cosmo/outgoing/live');
         // grab the file COSM_2023-03-26.gpg
         const cryptFile = list.find(file => file.name === 'COSM_2023-03-26.gpg');
+        // download the file
+        const downloadedFilePath = path.join(__dirname, cryptFile.name);
+        await sftp.get(`/dmc_cosmo/Cosmo/outgoing/live/${cryptFile.name}`, downloadedFilePath);
         // decrypt the file
-        //first download the file
-        await sftp.get(`/dmc_cosmo/Cosmo/outgoing/live/${cryptFile.name}`, path.join(__dirname, cryptFile.name));
-        // decrypt the file
-        let downloadedFile = path.join(__dirname, cryptFile.name);
-        console.log(downloadedFile);
-        const msg = await decryptFile(downloadedFile, './private_key.asc', './pub_key.asc');
-        //read msg file path
-        const file = fs.readFileSync(msg, 'utf8');
-        console.log(file);
+        const decryptedData = await decryptFile(downloadedFilePath, privateKeyPath, publicKeyPath);
 
-        return res.status(200).json({ success: true });
+        // Return the decrypted file content
+        return res.status(200).send(decryptedData);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error fetching file');
     }
 });
-async function streamWait(stream) {
-    return new Promise((resolve, reject) => {
-        stream.on('end', resolve);
-        stream.on('error', reject);
-    });
-}
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
