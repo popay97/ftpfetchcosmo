e3b597d460269f4463bb28723bf6d6046061e5b4
popay97 <5880963+popay97@users.noreply.github.com>
2023-04-06 23:27:58 +0200

    update

--- index.js
+++ index.js
@@ -185,3 +185,8 @@ app.post('/getEasyJetFilesFromFtp', async (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
