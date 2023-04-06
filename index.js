1724ad363638fac89ba108a172dddd578b0294cd
popay97 <5880963+popay97@users.noreply.github.com>
2023-04-07 00:14:33 +0200

    update

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === "OPTIONS") {
       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
